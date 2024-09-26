a8e8cc33558fc282a3772577c62342e9450c32e8
haifn7 <haifa72010@hotmail.com>
2024-09-26 17:06:53 +0300

    Initial Commit

---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by haifa on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project2Tests/Project2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project2Tests.swift
+//  Project2Tests
+//
+//  Created by haifa on 26/09/2024.
+//
+
+import Testing
+@testable import Project2
+
+struct Project2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
